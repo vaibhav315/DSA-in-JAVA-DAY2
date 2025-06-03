a2c06e495279b79356b048fc293efa6d3d0b730d
vaibhav315 <137273403+vaibhav315@users.noreply.github.com>
2025-06-03 21:20:51 +0530

    Update Solution.java

--- Solution.java
+++ Solution.java
@@ -1,33 +1,6 @@
 class Solution {
-// public int getSecondLargest(int[] arr) {
-//         // code here
-        
-//             if (arr.length<2) return -1;
-            
-//             int first= -1, second= -1;
-            
-//             for (int num:arr){
-//                 if (num>first){
-//                     second=first;
-//                     first=num;
-//                 } else if (num != first && num> second){
-//                     second = num;
-//                 }    
-                
-//             }
-//             return second;
-//         }
-//         public static void main (String[] args){
-//             Solution sol = new Solution();
-//             int[] arr1={10,20,4,45,99};
-//             int[] arr2={10,10,10};
-//             int[] arr3={5};
-//             int[] arr4={5,1};
-            
-//             System.out.println(sol.getSecondLargest(arr2));
-//         }
-//     }
-    
+
+
   void pushZerosToEnd(int[] arr) {
         int index = 0;
         for (int i = 0; i<arr.length; i++){
